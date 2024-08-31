ca7bb896c133fd9a1d030cd40366456aeecbe911
Dio Eracler <dioeracler1988@gmail.com>
2024-08-31 14:14:58 +0300

    deploy gh-pages

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import './assets/main.css'
-import './assets/NORMALIZE_STYLE.css'
-
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
--- src/types/types.ts
+++
@@ -1,5 +0,0 @@
-export interface ArticleI {
-    id: number,
-    title: string,
-    text: string,
-}
